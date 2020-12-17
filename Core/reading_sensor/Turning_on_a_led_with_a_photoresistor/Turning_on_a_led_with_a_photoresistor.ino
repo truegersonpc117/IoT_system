9d36032df2fafcd8ef1cd80a7e4990fec5abce1e
truegersonpc117 <66572555+truegersonpc117@users.noreply.github.com>
2020-12-17 13:10:42 -0600

    Commit inicial, entorno virtual y lectura de fotoresistor

---
+++ Core/reading_sensor/Turning_on_a_led_with_a_photoresistor/Turning_on_a_led_with_a_photoresistor.ino
@@ -0,0 +1,21 @@
+
+const int ledPin = 13;
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second to get an estimate of the value the arduino is reading:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+
+void loop() {
+  
+  int sensorValue = analogRead(A0);
+  
+  Serial.println(sensorValue);
+  if (sensorValue>=200) {
+    digitalWrite(ledPin, LOW);
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
+}
