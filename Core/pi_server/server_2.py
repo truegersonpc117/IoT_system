ef375696f19e7acbe62ffb764553223ad8344993
gersonpc117 <3065050290401@ingenieria.usac.edu.gt>
2020-12-21 20:04:19 -0600

    Version 2 of the server for the raspberry pi

---
+++ Core/pi_server/server_2.py
@@ -0,0 +1,38 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+IP_ADDR_ALL = '' 
+IP_PORT = 9800 
+
+BUFFER_SIZE = 16 * 1024 #16 KB blocks
+
+# Bind the socket to the port
+serverAddress = (IP_ADDR_ALL, IP_PORT) 
+print('Iniciando servidor en {}, puerto {}'.format(*serverAddress))
+sock.bind(serverAddress) 
+
+sock.listen(10) #Connections in queue
+
+while True:
+    # Waiting for connection
+    print('Waiting for remote connection')
+    connection, clientAddress = sock.accept()
+    try:
+        print('Connection from', clientAddress)
+
+        while True:
+            data = connection.recv(BUFFER_SIZE)
+            print('Received: {!r}'.format(data))
+            if data: #while receiving it won't close
+                print('Waiting for new message')
+            else:
+                print('Ending connection from', clientAddress)
+                break
+    
+    except KeyboardInterrupt:
+        sock.close()
+
+    finally:
+        # closing connection
+        connection.close()
